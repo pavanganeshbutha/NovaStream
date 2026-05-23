0a17ffca1fbf0775f510b94b54a3cca3f148918e
pavanganeshbutha <pavanganeshbutha@gmail.com>
2026-05-23 17:39:17 +0530

    implement routes

---
+++ src/constants/navigation.js
@@ -0,0 +1,18 @@
+export const NAV_LINKS = [
+  {
+    label: 'Movies',
+    path: '/movies',
+  },
+  {
+    label: 'TV Shows',
+    path: '/tv',
+  },
+  {
+    label: 'AI Picks',
+    path: '/ai',
+  },
+  {
+    label: 'Watchlist',
+    path: '/watchlist',
+  },
+];
